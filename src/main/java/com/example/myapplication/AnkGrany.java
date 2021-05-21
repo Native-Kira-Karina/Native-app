64d3e399f93e35d203864592fdd4b65aa389fd34
Native-Kira-Karina <kirastr354@gmail.com>
2021-05-21 11:56:07 +0300

    ...

--- src/main/java/com/example/myapplication/AnkGrany.java
+++ src/main/java/com/example/myapplication/AnkGrany.java
@@ -1,13 +1,39 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AnkGrany extends AppCompatActivity {
-
+    ImageButton arrow;
+    Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.anketa_grany);
+        arrow = (ImageButton) findViewById(R.id.arrow);
+        next = (Button) findViewById(R.id.SignUp);
+        View.OnClickListener clickarrow = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                i = new Intent(AnkGrany.this, Reg.class);
+                startActivity(i);
+            }
+        };
+        View.OnClickListener clnext = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                i = new Intent(AnkGrany.this, CheckNumber.class);
+                startActivity(i);
+            }
+        };
+        arrow.setOnClickListener(clickarrow);
+        next.setOnClickListener(clnext);
     }
 }
--- src/main/java/com/example/myapplication/First.java
+++ src/main/java/com/example/myapplication/First.java
@@ -1,17 +1,15 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCanceledListener;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -21,6 +19,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class First extends AppCompatActivity {
     private EditText edit_log, edit_pas;
     private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,13 +33,15 @@ public class First extends AppCompatActivity {
         edit_pas = findViewById(R.id.edit_pas);
         mAuth = FirebaseAuth.getInstance();
 
+        findViewById(R.id.registration).setOnClickListener(v -> onClickReg());
+        findViewById(R.id.recovery).setOnClickListener(v -> onClickSignIn());
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         FirebaseUser cUser = mAuth.getCurrentUser();
-        if(cUser != null){
+        if (cUser != null) {
             Toast.makeText(this, "Пользователь зарегестрирован", Toast.LENGTH_SHORT).show();
             cUser.reload();
         }
@@ -48,31 +49,36 @@ public class First extends AppCompatActivity {
     }
 
     public void onClickReg() {
-        if(!TextUtils.isEmpty(edit_log.getText().toString()) && !TextUtils.isEmpty(edit_pas.getText().toString())) {
+        Log.d("first onClickReg", "-> " + edit_log.getText() + " _ " + edit_pas.getText());
+        if (!TextUtils.isEmpty(edit_log.getText().toString()) && !TextUtils.isEmpty(edit_pas.getText().toString())) {
             mAuth.createUserWithEmailAndPassword(edit_log.getText().toString(), edit_pas.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){
+                    if (task.isSuccessful()) {
                         Toast.makeText(First.this, "Пользователь успешно зарегестрирован", Toast.LENGTH_SHORT).show();
-                    }else{
+                        Intent i;
+                        i = new Intent(First.this, Reg.class);
+                        startActivity(i);
+                    } else {
                         Toast.makeText(First.this, "Something went wrong", Toast.LENGTH_SHORT).show();
                     }
                 }
             });
-        }else{
+        } else {
             Toast.makeText(this, "Введите Email и пароль", Toast.LENGTH_SHORT).show();
         }
     }
 
 
     public void onClickSignIn() {
-        if(!TextUtils.isEmpty(edit_log.getText().toString()) && !TextUtils.isEmpty(edit_pas.getText().toString())) {
+        Log.d("first onClickSignIn", "-> " + edit_log.getText() + " _ " + edit_pas.getText());
+        if (!TextUtils.isEmpty(edit_log.getText().toString()) && !TextUtils.isEmpty(edit_pas.getText().toString())) {
             mAuth.signInWithEmailAndPassword(edit_log.getText().toString(), edit_pas.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){
+                    if (task.isSuccessful()) {
                         Toast.makeText(First.this, "Пользователь успешно вошёл", Toast.LENGTH_SHORT).show();
-                    }else {
+                    } else {
                         Toast.makeText(First.this, "Something went wrong", Toast.LENGTH_SHORT).show();
                     }
                 }
